c5b00bb7f5d5dcfa408601bff71711f2924f6702
nikit <nikitka.lyashko@bk.ru>
2023-04-29 17:05:04 +0300

    hello

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ window = display.set_mode([800, 600])
 
 text = pygame.font.SysFont("arial", 30, False, True)
 
+a=pygame.event.custom_type()
+
+pygame.time.set_timer(a,3000,1)
+
 while True:
     time.sleep(1/60)
 
@@ -17,6 +21,11 @@ while True:
 
     cobibtie_list = event.get()
     for cobibtie in cobibtie_list:
+
+        if cobibtie.type==a:
+            window.fill([211,219,5])
+            pygame.time.set_timer(a, random.randint(500,3000), 1)
+
         if cobibtie.type==pygame.QUIT:
             text_on_wind=text.render("НЕ закроюсь",True,[134,213,42])
             window.blit(text_on_wind,[220,300])
@@ -41,8 +50,20 @@ while True:
 
             window.blit(mouse_1, [cobibtie.pos[0], cobibtie.pos[1]])
 
-        if cobibtie.type==pygame.MOUSEMOTION:
-            pygame.draw.circle(window, [145, 23, 72], [cobibtie.pos[0],cobibtie.pos[1]], 10, 3)
+
+        if cobibtie.type==pygame.MOUSEMOTION and cobibtie.buttons[0]==1:
+            pygame.draw.circle(window, [225, 0, 0],cobibtie.pos, 10, 3)
+
+        elif cobibtie.type==pygame.MOUSEMOTION and cobibtie.buttons[1]==1:
+            pygame.draw.circle(window, [0,225, 0],cobibtie.pos, 10, 3)
+
+        elif cobibtie.type==pygame.MOUSEMOTION and cobibtie.buttons[2]==1:
+            pygame.draw.circle(window, [0,0, 225], cobibtie.pos, 10, 3)
+
+        elif cobibtie.type==pygame.MOUSEMOTION:
+            pygame.draw.circle(window, [145, 23, 72],cobibtie.pos, 1, 1 )
+
+
 
 
 
